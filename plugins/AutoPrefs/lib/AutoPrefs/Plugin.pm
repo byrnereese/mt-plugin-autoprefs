f27dc28614c2d7db18f82df75940f9111fcf4a31
Byrne <byrne@majordojo.com>
2009-10-15 10:01:22 -0700

    added a preferences chooser screen to assist in the automatic application of a config

--- plugins/AutoPrefs/lib/AutoPrefs/Plugin.pm
+++ plugins/AutoPrefs/lib/AutoPrefs/Plugin.pm
@@ -2,6 +2,60 @@ package AutoPrefs::Plugin;
 
 use strict;
 
+sub apply {
+    my $app     = shift;
+    my ($param) = @_;
+
+    $app->validate_magic or return;
+
+    my $q       = $app->{query};
+    my $blog    = MT->model('blog')->load( $q->param('blog_id') );
+    my $pid     = $q->param('pref_id');
+
+    _apply_prefs( $blog, $pid );
+    
+    return $app->redirect(
+	$app->uri(
+	    'mode' => 'autoprefs_chooser',
+	    'args' => {
+		'prefs_applied' => 1,
+		'blog_id'       => $app->param('blog_id'),
+		'return_args'   => $app->return_args,
+		'magic_token'   => $app->param('magic_token')
+	    }
+	)
+    );
+}
+
+sub chooser {
+    my $app     = shift;
+    my ($param) = @_;
+    my $q       = $app->{query};
+    my $blog    = MT->model('blog')->load( $q->param('blog_id') );
+
+    $param ||= {};
+
+    my $prefs = MT->registry('blog_preferences');
+    my @data;
+    foreach my $pid (keys %$prefs) {
+	my $pref = $prefs->{$pid};
+	push @data, {
+	    id          => $pid,
+	    name        => &{ $pref->{'label'} },
+            description => $pref->{'description'},
+            order       => $pref->{'order'} || 10,
+	};
+    }
+    @data = sort { $a->{order} <=> $b->{order} } @data;
+    $param->{prefs} = \@data;
+    $param->{blog_id} = $app->param('blog_id');
+    $param->{return_args} = $app->return_args;
+    $param->{magic_token} = $app->param('magic_token');
+    $param->{prefs_applied} = $app->param('prefs_applied') ? 1 : 0;
+
+    return $app->load_tmpl( 'chooser.tmpl', $param );
+}
+
 sub on_template_set_change {
    my ( $cb, $param ) = @_;
    my $blog = $param->{blog} or return;
@@ -12,32 +66,34 @@ sub on_template_set_change {
    my $pref_id = MT->registry('template_sets')->{$ts}->{blog_preferences};
    return undef unless $pref_id;
 
-   MT->log({
-       blog_id => $blog->id,
-       message => "The AutoPrefs plugin is configuring " . $blog->name,
-       level => MT::Log::INFO(),
-   });
+   _apply_prefs( $blog, $pref_id );
+}
 
-   my $prefs = MT->registry('blog_preferences')->{$pref_id}->{'preferences'};
-   foreach my $col (keys %$prefs) {
-       my $value = $prefs->{$col};
-       if ($blog->has_column($col)) {
-	   # TODO Validate input
-	   $blog->$col($value);
-#	   MT->log({
-#	       blog_id => $blog->id,
-#	       message => "The AutoPrefs plugin set $col to '$value.'",
-#	       level => MT::Log::INFO(),
-#	   });
-       } else {
-	   MT->log({
-	       blog_id => $blog->id,
-	       message => "The AutoPrefs plugin tried to set a blog preference $col that does not exist.",
-	       level => MT::Log::WARNING(),
-	   });
-       }
-   }
-   $blog->save;
+sub _apply_prefs {
+    my ($blog, $pid) = @_;
+
+    my $label = &{ MT->registry('blog_preferences')->{$pid}->{label} };
+    MT->log({
+	blog_id => $blog->id,
+	message => "The AutoPrefs plugin is configuring " . $blog->name . " with $label.",
+	level => MT::Log::INFO(),
+    });
+
+    my $prefs = MT->registry('blog_preferences')->{$pid}->{'preferences'};
+    foreach my $col (keys %$prefs) {
+	my $value = $prefs->{$col};
+	if ($blog->has_column($col)) {
+	    # TODO Validate input
+	    $blog->$col($value);
+	} else {
+	    MT->log({
+		blog_id => $blog->id,
+		message => "The AutoPrefs plugin tried to set a blog preference $col that does not exist.",
+		level => MT::Log::WARNING(),
+		    });
+	}
+    }
+    $blog->save;
 }
 
 1;
