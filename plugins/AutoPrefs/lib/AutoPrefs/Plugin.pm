115a7a60d7fc20fc492cfb75b0de982462ed88ca
Byrne <byrne@majordojo.com>
2009-10-14 23:40:23 -0700

    got the prototype working and began documentation of allowable config parameters

--- plugins/AutoPrefs/lib/AutoPrefs/Plugin.pm
+++ plugins/AutoPrefs/lib/AutoPrefs/Plugin.pm
@@ -5,8 +5,39 @@ use strict;
 sub on_template_set_change {
    my ( $cb, $param ) = @_;
    my $blog = $param->{blog} or return;
-   
-#   $blog->save;
+
+   my $ts   = $blog->template_set;
+   return undef unless $ts;
+
+   my $pref_id = MT->registry('template_sets')->{$ts}->{blog_preferences};
+   return undef unless $pref_id;
+
+   MT->log({
+       blog_id => $blog->id,
+       message => "The AutoPrefs plugin is configuring " . $blog->name,
+       level => MT::Log::INFO(),
+   });
+
+   my $prefs = MT->registry('blog_preferences')->{$pref_id}->{'preferences'};
+   foreach my $col (keys %$prefs) {
+       my $value = $prefs->{$col};
+       if ($blog->has_column($col)) {
+	   # TODO Validate input
+	   $blog->$col($value);
+#	   MT->log({
+#	       blog_id => $blog->id,
+#	       message => "The AutoPrefs plugin set $col to '$value.'",
+#	       level => MT::Log::INFO(),
+#	   });
+       } else {
+	   MT->log({
+	       blog_id => $blog->id,
+	       message => "The AutoPrefs plugin tried to set a blog preference $col that does not exist.",
+	       level => MT::Log::WARNING(),
+	   });
+       }
+   }
+   $blog->save;
 }
 
 1;
